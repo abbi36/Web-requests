27b8dee9bdb4b4fce716997b5e0dae561a020f28
Abbi Engel <abbi.engel33@gmail.com>
2022-03-01 14:01:56 -0600

    commit 2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import got from 'got';
 
 async function main() {
-    const resp = await got('https://httpbin.org/get').json();
+    const resp = await got('https://api.github.com/users/abbi36/repos').json();
     console.log(resp);
 }
 
